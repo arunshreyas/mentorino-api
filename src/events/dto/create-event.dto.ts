01b25bb7e77bd144f68a06c79917acb68330b80e
Arun Shreyas <arunshreyasbhattiprolu10@gmail.com>
2026-05-03 21:42:50 +0530

    event dtos created

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,1 +1,26 @@
-export class CreateEventDto {}
+import { IsArray, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateEventDto {
+    @IsNotEmpty()
+    @IsString()
+    title: string;
+    @IsNotEmpty()
+    @IsString()
+    date: string;
+    @IsNotEmpty()
+    @IsString()
+    time: string;
+    @IsNotEmpty()
+    @IsString()
+    location: string;
+    @IsNotEmpty()
+    @IsString()
+    description: string;
+    @IsNotEmpty()
+    @IsString()
+    link: string;
+    @IsNotEmpty()
+    @IsArray()
+    @IsString({ each: true })
+    attendees?: string[];
+}
--- src/events/dto/update-event.dto.ts
+++ src/events/dto/update-event.dto.ts
@@ -1,4 +1,28 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateEventDto } from './create-event.dto';
+import { IsArray, IsOptional, IsString } from 'class-validator';
 
-export class UpdateEventDto extends PartialType(CreateEventDto) {}
+export class UpdateEventDto extends PartialType(CreateEventDto) {
+    @IsOptional()
+    @IsString()
+    title?: string;
+    @IsOptional()
+    @IsString()
+    date?: string;
+    @IsOptional()
+    @IsString()
+    time?: string;
+    @IsOptional()
+    @IsString()
+    location?: string;
+    @IsOptional()
+    @IsString()
+    description?: string;
+    @IsOptional()
+    @IsString()
+    link?: string;
+    @IsOptional()
+    @IsArray()
+    @IsString({ each: true })
+    attendees?: string[];
+}
