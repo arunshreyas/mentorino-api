f8801d1b68d91d3ddaf77e0d7158da5e311cea93
Arun Shreyas <arunshreyasbhattiprolu10@gmail.com>
2026-05-03 21:39:06 +0530

    Dto's created for bookings module

--- src/bookings/dto/create-booking.dto.ts
+++ src/bookings/dto/create-booking.dto.ts
@@ -1,1 +1,22 @@
-export class CreateBookingDto {}
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+export class CreateBookingDto {
+    @IsNotEmpty()
+    @IsString()
+    user_id: string;
+    @IsNotEmpty()
+    @IsString()
+    user_name: string;
+    @IsNotEmpty()
+    @IsString()
+    date: string;
+    @IsNotEmpty()
+    @IsString()
+    time: string;
+    @IsOptional()
+    @IsString()
+    status: "upcoming" | "completed" | "cancelled" = "upcoming";
+    @IsOptional()
+    @IsString()
+    notes?: string;
+}
--- src/bookings/dto/update-booking.dto.ts
+++ src/bookings/dto/update-booking.dto.ts
@@ -1,4 +1,24 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateBookingDto } from './create-booking.dto';
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateBookingDto extends PartialType(CreateBookingDto) {}
+export class UpdateBookingDto extends PartialType(CreateBookingDto) {
+    @IsOptional()
+    @IsString()
+    status?: "upcoming" | "completed" | "cancelled";
+    @IsOptional()
+    @IsString()
+    notes?: string;
+    @IsOptional()
+    @IsString()
+    user_name?: string;
+    @IsOptional()
+    @IsString()
+    user_id?: string;
+    @IsOptional()
+    @IsString()
+    date?: string;
+    @IsOptional()
+    @IsString()
+    time?: string;
+}
